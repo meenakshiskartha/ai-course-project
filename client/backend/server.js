6f024388998caa221c7eded6d712c597876bb97d
Meenakshi <meenakshiskartha@gmail.com>
2026-04-30 13:02:38 +0530

    added root route

--- client/backend/server.js
+++ client/backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend is running");
+  res.status(200).send("Server is running");
 });
 // ✅ dashboard
 app.get("/dashboard", async (req, res) => {
